e54b25b5054e11d2789b69bb4ae3988e4aa4dbbf
ryo4004 <kato@ryo4004.net>
2022-04-08 16:33:16 +0000

    スクロールガイドをコンポーネントに切り出し

---
+++ components/index/ScrollGuide.tsx
@@ -0,0 +1,5 @@
+import styles from './ScrollGuide.module.scss'
+
+export const ScrollGuide = () => {
+  return <span className={styles['scroll-guide']}></span>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import styles from '../styles/index.module.scss'
-
-import type { NextPage } from 'next'
 import { MainBackgroundImage } from '../components/index/MainBackgroundImage'
 import { MainLogo } from '../components/index/MainLogo'
 import { Quote } from '../components/index/Quote'
+import { ScrollGuide } from '../components/index/ScrollGuide'
+
+import styles from '../styles/index.module.scss'
+
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
@@ -12,7 +14,7 @@ const Home: NextPage = () => {
         <MainBackgroundImage />
         <MainLogo showConcertGuide={true} />
         <Quote />
-        <span className={styles['scroll-guide']}></span>
+        <ScrollGuide />
       </header>
     </>
   )
