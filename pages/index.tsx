006181c183cd6f00e57b732200004cd763814221
ryo4004 <kato@ryo4004.net>
2022-04-08 16:27:57 +0000

    ロゴと演奏会のお知らせをコンポーネントに切り出し

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,15 @@
-import Link from 'next/link'
-
-import { Logo } from '../components/svg/Logo'
-
 import styles from '../styles/index.module.scss'
 
 import type { NextPage } from 'next'
 import { MainBackgroundImage } from '../components/index/MainBackgroundImage'
+import { MainLogo } from '../components/index/MainLogo'
 
 const Home: NextPage = () => {
   return (
     <>
       <header className={styles.header}>
         <MainBackgroundImage />
-        <section className={styles['main-logo']}>
-          <main className={styles['before-direct-link']}>
-            <Logo />
-          </main>
-          <div className={styles['direct-link']}>
-            <Link href="#">
-              <a>
-                <div className={styles.detail}>
-                  <p>演奏会のお知らせ</p>
-                  <h3>定期演奏会</h3>
-                </div>
-              </a>
-            </Link>
-          </div>
-        </section>
+        <MainLogo showConcertGuide={true} />
         <section className={styles.quote}>
           <q>
             <Quote />
