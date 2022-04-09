123048b5a0306f0838e39ff5d47b65bff86efbd8
ryo4004 <kato@ryo4004.net>
2022-04-09 05:06:42 +0000

    useFetchを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,16 @@ import { ScrollGuide } from '../components/index/ScrollGuide'
 import { News } from '../components/index/News'
 import { Introduction } from '../components/index/Introduction'
 import { Concert } from '../components/index/Concert'
+import useFetch from '../utilities/hooks/useFetch'
+import { API_BASE_PATH, SCHEDULE_API } from '../utilities/constant'
 
 import styles from '../styles/index.module.scss'
 
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
+  const { loading, data } = useFetch(API_BASE_PATH + SCHEDULE_API)
+  console.log({ loading, data })
   return (
     <>
       <header className={styles.header}>
