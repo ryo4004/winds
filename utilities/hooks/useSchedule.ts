d66590d35a2f82192690edbdf9188c197b9ac3ea
ryo4004 <kato@ryo4004.net>
2022-04-09 07:18:40 +0000

    コードを整理

--- utilities/hooks/useSchedule.ts
+++ utilities/hooks/useSchedule.ts
@@ -3,9 +3,9 @@ import { DateTime } from 'luxon'
 import useFetch from './useFetch'
 
 import { API_BASE_PATH, SCHEDULE_API } from '../constant'
-import { createPracticeSchedule } from '../domain/schedule/scheduleService'
+import { createSchedule } from '../domain/schedule/createSchedule'
 
-import type { ApiSchedule, ApiScheduleItem } from '../types/schedule'
+import type { ApiSchedule } from '../types/schedule'
 import type { PracticeScheduleItem } from '../domain/schedule/scheduleService'
 
 export type Schedule = {
@@ -25,26 +25,4 @@ export const useSchedule = () => {
   }, [data])
 
   return { loading, schedule }
-}
-
-const createSchedule = (schedule: ApiSchedule): Schedule => {
-  const list = schedule.list.map((item) => {
-    return createScheduleItem(item)
-  })
-  return {
-    next: createScheduleItem(schedule.next),
-    list,
-  }
-}
-
-const createScheduleItem = (item: ApiScheduleItem): PracticeScheduleItem => {
-  const startAt = item.date + 'T' + item.time.start + '+09:00'
-  const endAt = item.date + 'T' + item.time.end + '+09:00'
-  return createPracticeSchedule({
-    startAt: DateTime.fromISO(startAt),
-    endAt: DateTime.fromISO(endAt),
-    place: item.place,
-    studio: item.studio,
-    memo: item.memo || '',
-  })
 }
