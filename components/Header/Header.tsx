88fe4e2488784cd6aba2e2348f842b8859907ec7
ryo4004 <kato@ryo4004.net>
2022-04-13 16:54:25 +0000

    ヘッダーを追加

---
+++ components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import { Logo } from '../svg/Logo'
+
+import styles from './Header.module.scss'
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <section className={styles['main-logo']}>
+        <Link href="/">
+          <Logo />
+        </Link>
+      </section>
+    </header>
+  )
+}
