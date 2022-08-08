2a0db85999d442c8223d5828bcd9c00a33bf847a
ryo4004 <kato@ryo4004.net>
2022-08-08 21:32:56 +0900

    タイムゾーンの指定を追加

--- utilities/mail/mail.ts
+++ utilities/mail/mail.ts
@@ -35,7 +35,7 @@ export const createNoticeMessage = (body: MailForm) => {
   return `winds-n.com (PC) にて下記のお問い合わせがありました。
 
 受付日時: ${DateTime.now().setZone('Asia/Tokyo').toFormat('yyyy/MM/dd HH:mm')}
-返信目安: ${DateTime.now().plus({ day: 7 }).toFormat('yyyy/MM/dd HH:mm')}(一週間)
+返信目安: ${DateTime.now().setZone('Asia/Tokyo').plus({ day: 7 }).toFormat('yyyy/MM/dd HH:mm')}(一週間)
 
 アドレス確認: 送信成功
 
