35e57d81f43b3786ac94715e7da2aff4c61606dd
ryo4004 <kato@ryo4004.net>
2022-08-10 01:34:18 +0900

    練習日程ページにレイアウトを追加

--- pages/schedule.tsx
+++ pages/schedule.tsx
@@ -1,5 +1,26 @@
+import { Block } from '../components/Layout/Block'
+import { Layout } from '../components/Layout/Layout'
+
 const Schedule = () => {
-  return <>Schedule</>
+  return (
+    <Layout pageTitle="練習日程" pageSubTitle="Schedule" pathList={[{ path: '/schedule', label: '練習日程' }]}>
+      <Block title="次回の練習日" subTitle="Next Practice">
+        <div className="contents">
+          <div className="schedule-next">
+            <div></div>
+          </div>
+        </div>
+      </Block>
+
+      <Block title="今後の練習日程" subTitle="Schedule">
+        <div className="contents">
+          <div className="schedule-after">
+            <div></div>
+          </div>
+        </div>
+      </Block>
+    </Layout>
+  )
 }
 
 export default Schedule
