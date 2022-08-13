84467fcb19610f031482d25c09631822f3e9dc5d
ryo4004 <kato@ryo4004.net>
2022-08-13 18:06:32 +0900

    過去の演奏会ページの本文を追加

--- components/Layout/Block.tsx
+++ components/Layout/Block.tsx
@@ -1,12 +1,14 @@
 import type { ReactNode } from 'react'
 import styles from './Block.module.scss'
 
-export const Block = ({ children, title, subTitle }: { children: ReactNode; title: string; subTitle: string }) => {
+export const Block = ({ children, title, subTitle }: { children: ReactNode; title?: string; subTitle?: string }) => {
   return (
     <div className={styles.block}>
-      <div className={styles.title}>
-        <h2 data-subttl={subTitle}>{title}</h2>
-      </div>
+      {title && subTitle && (
+        <div className={styles.title}>
+          <h2 data-subttl={subTitle}>{title}</h2>
+        </div>
+      )}
       <div className={styles.contents}>{children}</div>
     </div>
   )
--- pages/history.tsx
+++ pages/history.tsx
@@ -1,9 +1,23 @@
+import { Block } from '../components/Layout/Block'
+import { Layout } from '../components/Layout/Layout'
+import { Text } from '../components/Layout/Text'
 import { useHistory } from './api/api'
 
 const History = () => {
   const { data, error } = useHistory()
   console.log({ data, error })
-  return <>History</>
+  return (
+    <Layout pageTitle="過去の演奏会" pageSubTitle="History" pathList={[{ path: '/history', label: '過去の演奏会' }]}>
+      <Block>
+        <Text>
+          <p>
+            ウィンズは1988年5月に第1回定期演奏会を開催してから現在に至るまで毎年秋に定期演奏会を開催しています。
+            また、春にはミニコンサートも開催しております。 ここではこれまでに開催した演奏会の記録を紹介します。
+          </p>
+        </Text>
+      </Block>
+    </Layout>
+  )
 }
 
 export default History
